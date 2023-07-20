2ffc90f926d6cf9aaa308a350b7740e26c99b873
Nozimjon Nabiev <73819566+NozimjonNabiev@users.noreply.github.com>
2023-07-21 01:33:58 +0500

    Done

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -2,8 +2,11 @@ package com.epam.demo;
 
 import java.util.List;
 
+import com.epam.utils.StringUtils;
+
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
        //magic happens here
+        return args.stream().allMatch(StringUtils::isPositiveNumber);
     }
 }
